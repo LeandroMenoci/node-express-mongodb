1f0473ef3ecde9e37c6fa5e22aa677d6161b8e9d
leandromenoci <menoci.leandro@gmail.com>
2022-09-02 17:58:40 -0300

    Método DELETE

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -40,6 +40,17 @@ class LivroController {
       }
     })
   }
+
+  static excluirLivro = (req, res) => {
+    const id = req.params.id
+    livros.findByIdAndDelete(id, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Livro removido com sucesso'})
+      } else {
+        res.status(500).send({message: `${err.message} - não foi possível excluir`})
+      }
+    })
+  }
 }
 
 export default LivroController
